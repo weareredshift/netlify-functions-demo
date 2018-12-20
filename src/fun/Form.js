a78aa45c6696cfcf755c51a340304291c80b7272
Sasha Klein <sashafklein@gmail.com>
2018-12-19 16:46:30 -0800

    Fixed form?

--- src/fun/Form.js
+++ src/fun/Form.js
@@ -3,11 +3,11 @@ import React from 'react';
 const Form = () => (
   <div>
     <p>Check out this form:</p>
-    <form style={{ margin: '20px' }} netlify name="contacts">
+    <form style={{ margin: '20px' }} netlify name="contacts" method="post">
       <input type="text" name="name" placeholder="Name" />
       <input type="text" name="email" placeholder="Email" />
       <br/>
-      <input type="submit" />
+      <button type="submit">Send</button>
     </form>
   </div>
 );
