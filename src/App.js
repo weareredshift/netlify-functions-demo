219081461e869913629d8043900dcd6db6b26909
Sasha Klein <sashafklein@gmail.com>
2018-12-19 16:40:23 -0800

    Lotsa tests

--- src/App.js
+++ src/App.js
@@ -1,59 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      input: ''
-    }
-  }
+import Requester from './fun/Requester';
+import Identity from './fun/Identity';
+import Form from './fun/Form';
 
-  makeRequest() {
-    const { input } = this.state;
-    const endpoint = '/.netlify/functions/hello';
-    const finalEndpoint = input ? `${endpoint}?input=${input}` : endpoint;
-
-    fetch(finalEndpoint)
-      .then(response => response.json())
-      .then(result => {
-        this.setState({ result })
-      })
-  }
-
-  render() {
-    const { result, input } = this.state;
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-
-          <input
-            value={ input }
-            onChange={ (e) => { this.setState({ input: e.target.value }) }}
-            placeholder="Submit a query string"
-            onKeyPress={ event => {
-              if (event.key === 'Enter') {
-                this.makeRequest();
-              }
-            }}
-          />
-
-          <p>Result:</p>
-          { result &&
-            <pre>
-              { JSON.stringify(result) }
-            </pre>
-          }
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <Identity />
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>Let's get started!</p>
+      <Requester />
+      <Form />
+    </header>
+  </div>
+);
 
 export default App;
---
+++ src/fun/Identity.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Identity = () => (
+  <div data-netlify-identity-button style={{ fontSize: '16px' }}>
+    Login with Netlify Identity
+  </div>
+)
+
+export default Identity;
